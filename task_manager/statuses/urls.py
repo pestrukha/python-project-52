660ade05aae1aa3f570e92c3c9e86290a02f6f32
pestrukha <sturbauen@gmail.com>
2024-09-24 14:24:16 +0400

    add StatusUpdateView

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -5,4 +5,5 @@ from task_manager.statuses import views
 urlpatterns = [
     path('', views.StatusesListView.as_view(), name='statuses_list'),
     path('create/', views.StatusCreateView.as_view(), name='status_create'),
+    path('<int:pk>/update/', views.StatusUpdateView.as_view(), name='status_update'),
 ]
--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from task_manager.mixins import NoLoginMixin
 from task_manager.statuses.models import Status
 from django.contrib.messages.views import SuccessMessageMixin
@@ -27,4 +27,18 @@ class StatusCreateView(NoLoginMixin,
     extra_context = {
         'title': 'Создать статус',
         'button_text': 'Создать',
+    }
+
+
+class StatusUpdateView(NoLoginMixin,
+                       SuccessMessageMixin,
+                       UpdateView):
+    model = Status
+    form_class = StatusForm
+    template_name = 'statuses/status_update.html'
+    success_message = 'Статус успешно изменён'
+    success_url = reverse_lazy('statuses_list')
+    extra_context = {
+        'title': 'Изменить статус',
+        'button_text': 'Изменить',
     }
