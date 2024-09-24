a90757b0e958a8de9593d991453b9eb2b15f27a1
pestrukha <sturbauen@gmail.com>
2024-09-24 14:04:20 +0400

    add StatusCreateView

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,13 +1,30 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from task_manager.mixins import NoLoginMixin
 from task_manager.statuses.models import Status
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from task_manager.statuses.forms import StatusForm
 
 
 class StatusesListView(NoLoginMixin,
                        ListView):
     model = Status
-    template_name = 'statuses/statuses_list.html'
+    template_name = 'statuses/status_list.html'
     context_object_name = 'statuses'
     extra_context = {
         'title': 'Статусы'
+    }
+
+
+class StatusCreateView(NoLoginMixin,
+                       SuccessMessageMixin,
+                       CreateView):
+    model = Status
+    form_class = StatusForm
+    template_name = 'statuses/status_create.html'
+    success_message = 'Статус успешно создан'
+    success_url = reverse_lazy('statuses_list')
+    extra_context = {
+        'title': 'Создать статус',
+        'button_text': 'Создать',
     }
