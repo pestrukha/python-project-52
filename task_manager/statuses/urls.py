a8dadcfdc1672f258f7968691a12d0889655b6b6
pestrukha <sturbauen@gmail.com>
2024-09-24 14:47:56 +0400

    add StatusDeleteView

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.StatusesListView.as_view(), name='statuses_list'),
     path('create/', views.StatusCreateView.as_view(), name='status_create'),
     path('<int:pk>/update/', views.StatusUpdateView.as_view(), name='status_update'),
+    path('<int:pk>/delete/', views.StatusDeleteView.as_view(), name='status_delete'),
 ]
--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,9 +1,12 @@
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from task_manager.mixins import NoLoginMixin
 from task_manager.statuses.models import Status
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from task_manager.statuses.forms import StatusForm
+from django.db.models import ProtectedError
+from django.contrib import messages
+from django.shortcuts import redirect
 
 
 class StatusesListView(NoLoginMixin,
@@ -42,3 +45,25 @@ class StatusUpdateView(NoLoginMixin,
         'title': 'Изменить статус',
         'button_text': 'Изменить',
     }
+
+
+class StatusDeleteView(NoLoginMixin,
+                       SuccessMessageMixin,
+                       DeleteView):
+    model = Status
+    template_name = 'statuses/status_delete.html'
+    success_message = 'Статус успешно удалён'
+    success_url = reverse_lazy('statuses_list')
+    protected_message = 'Невозможно удалить статус, потому что он используется'
+    protected_url = reverse_lazy('statuses_list')
+    extra_context = {
+        'title': 'Удалить статус',
+        'button_text': 'Удалить',
+    }
+
+    def post(self, request, *args, **kwargs):
+        try:
+            return super().post(request, *args, **kwargs)
+        except ProtectedError:
+            messages.error(request, self.protected_message)
+            return redirect(self.protected_url)
