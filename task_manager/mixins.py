db3e2ce6e5719941dc539f8966b2c9bb69287d4c
pestrukha <sturbauen@gmail.com>
2024-09-26 14:38:45 +0400

    fix TaskDeleteView

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -8,15 +8,3 @@ class NoLoginMixin(LoginRequiredMixin):
     def handle_no_permission(self):
         messages.error(self.request, 'Вы не авторизованы! Пожалуйста, выполните вход')
         return redirect(reverse_lazy('login'))
-
-
-class AuthorMixin(UserPassesTestMixin):
-    author_permission_message = ''
-    author_permission_url = ''
-
-    def test_func(self):
-        return self.get_object().author == self.request.user
-
-    def handle_no_permission(self):
-        messages.error(self.request, self.author_permission_message)
-        return redirect(self.author_permission_url)
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,5 +1,3 @@
-from django.contrib.messages.views import SuccessMessageMixin
-
 from task_manager.mixins import NoLoginMixin
 from django_filters.views import FilterView
 from task_manager.tasks.models import Task
@@ -9,7 +7,8 @@ from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from task_manager.tasks.forms import TaskForm
 from django.urls import reverse_lazy
 from django.contrib.auth import get_user_model
-from task_manager.mixins import AuthorMixin
+from django.contrib import messages
+from django.shortcuts import redirect
 
 
 User = get_user_model()
@@ -49,11 +48,26 @@ class TaskUpdateView(NoLoginMixin, SuccessMessageMixin, UpdateView):
     success_message = 'Задача успешно изменена'
 
 
-class TaskDeleteView(NoLoginMixin, AuthorMixin,
-                     SuccessMessageMixin, DeleteView):
+class TaskDeleteView(NoLoginMixin, SuccessMessageMixin, DeleteView):
     template_name = 'tasks/task_delete.html'
     model = Task
     success_url = reverse_lazy('task_list')
     success_message = 'Задача успешно удалена'
-    author_permission_message = 'Задачу может удалить только её автор'
-    author_permission_url = reverse_lazy('task_list')
+
+    def check_task_author(self):
+        if self.get_object().author != self.request.user:
+            messages.error(
+                self.request,
+                'Задачу может удалить только её автор')
+            return False
+        return True
+
+    def get(self, request, *args, **kwargs):
+        if not self.check_task_author():
+            return redirect('task_list')
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        if not self.check_task_author():
+            return redirect('task_list')
+        return super().post(request, *args, **kwargs)
