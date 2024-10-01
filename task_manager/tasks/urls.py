6bd2519a28e5f00c267fa7335ea4934fe5e8c37b
pestrukha <sturbauen@gmail.com>
2024-10-01 22:28:41 +0400

    fix flake8

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from task_manager.tasks.views import TasksListView, TaskCreateView, TaskView, TaskUpdateView, TaskDeleteView
+from task_manager.tasks.views import (TasksListView,
+                                      TaskCreateView,
+                                      TaskView,
+                                      TaskUpdateView,
+                                      TaskDeleteView)
 
 urlpatterns = [
     path('', TasksListView.as_view(), name='task_list'),
