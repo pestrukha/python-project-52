98cb2c5163ef222e3bb6af60b0f838bb5db18624
pestrukha <sturbauen@gmail.com>
2024-10-01 21:43:04 +0400

    fix User model

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+def get_full_name(self):
+    return f'{self.first_name} {self.last_name}'
+
+
+get_user_model().add_to_class('__str__', get_full_name)
