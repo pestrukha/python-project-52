7202f85e6cfbcd4a18cef0d7e42f8c72269cfebe
pestrukha <sturbauen@gmail.com>
2024-10-01 21:16:05 +0400

    fix TaskForm

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,9 +1,22 @@
 from django import forms
 from task_manager.tasks.models import Task
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 class TaskForm(forms.ModelForm):
+    executor = forms.ModelChoiceField(
+        queryset=User.objects.all(),
+        label='Исполнитель',
+        widget=forms.Select,
+    )
 
     class Meta:
         model = Task
         fields = ('name', 'description', 'status', 'executor', 'labels')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['executor'].label_from_instance = lambda obj: f'{obj.first_name} {obj.last_name}'
