95900a7ca821a837921f1c2cb31cc026a8c358f8
pestrukha <sturbauen@gmail.com>
2024-10-01 21:35:45 +0400

    fix TaskForm 2

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -2,9 +2,7 @@ from django import forms
 from task_manager.tasks.models import Task
 from django.contrib.auth import get_user_model
 
-
 User = get_user_model()
-
 
 class TaskForm(forms.ModelForm):
     executor = forms.ModelChoiceField(
@@ -19,4 +17,4 @@ class TaskForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['executor'].label_from_instance = lambda obj: f'{obj.first_name} {obj.last_name}'
+        self.fields['executor'].label_from_instance = lambda obj: obj.get_full_name()
