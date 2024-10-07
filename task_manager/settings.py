adb0bf98f7d6a18e125fbe8fe3aa1daa49d84134
pestrukha <sturbauen@gmail.com>
2024-10-07 15:33:35 +0400

    try rollbar

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
 ]
 
 ROOT_URLCONF = 'task_manager.urls'
@@ -135,3 +136,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ROLLBAR = {
+    'access_token': os.getenv('ROLLBAR_TOKEN'),
+
+    'environment': 'development' if DEBUG else 'production',
+    'code_version': '1.0',
+    'root': BASE_DIR,
+}
--- task_manager/tasks/tests.py
+++ task_manager/tasks/tests.py
@@ -13,6 +13,8 @@ class TaskTestCase(TestCase):
         self.client.force_login(self.user)
         self.tasks_url = reverse('task_list')
         self.task_create_url = reverse('task_create')
+        self.task_update_url = reverse('task_update', args=[1])
+        self.task_delete_url = reverse('task_delete', args=[1])
         self.task1 = Task.objects.get(pk=1)
         self.task2 = Task.objects.get(pk=2)
         self.task3 = Task.objects.get(pk=3)
@@ -52,3 +54,55 @@ class TaskTestCase(TestCase):
 
         self.assertEqual(task.labels.count(), 1)
         self.assertEqual(task.labels.first().id, 1)
+
+    def test_update_task(self):
+        updated_data = {
+            'name': 'updated task',
+            'status': 2,
+            'description': 'updated description',
+            'executor': 2,
+            'labels': 2
+        }
+
+        response = self.client.post(self.task_update_url, data=updated_data)
+
+        self.assertRedirects(response, self.tasks_url)
+
+        self.task1.refresh_from_db()
+        self.assertEqual(self.task1.name, 'updated task')
+        self.assertEqual(self.task1.description, 'updated description')
+        self.assertEqual(self.task1.status.id, 2)
+        self.assertEqual(self.task1.executor.id, 2)
+        self.assertEqual(self.task1.labels.count(), 1)
+        self.assertEqual(self.task1.labels.first().id, 2)
+
+    def test_delete_task(self):
+        response = self.client.post(self.task_delete_url)
+
+        self.assertRedirects(response, self.tasks_url)
+
+        self.assertFalse(Task.objects.filter(pk=self.task1.pk).exists())
+
+    def test_filter_tasks(self):
+        response = self.client.get(f'{self.tasks_url}?status=1&executor=1&label=')
+        tasks_list = response.context['tasks']
+
+        self.assertEqual(len(tasks_list), 1)
+        task = tasks_list[0]
+        self.assertEqual(task.name, 'Питон')
+        self.assertEqual(task.executor.id, 1)
+        self.assertEqual(task.status.id, 1)
+
+    def test_one_task(self):
+        self.one_task_url = reverse('one_task', args=[1])
+
+        response = self.client.get(self.one_task_url)
+        self.assertEqual(response.status_code, 200)
+
+        task = response.context['task']
+        self.assertEqual(task.name, self.task1.name)
+        self.assertEqual(task.description, self.task1.description)
+        self.assertEqual(task.author.id, self.task1.author.id)
+        self.assertEqual(task.executor.id, self.task1.executor.id)
+        self.assertEqual(task.status.id, self.task1.status.id)
+        self.assertEqual(task.created_at, self.task1.created_at)
